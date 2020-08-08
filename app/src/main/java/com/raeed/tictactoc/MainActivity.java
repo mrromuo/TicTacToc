f1d4ba28e8f5d9d3f2e0d3ebd8fd0a34d977433c
mrromuo <48122087+mrromuo@users.noreply.github.com>
2020-08-08 17:38:14 +0300

    Main Mune added

--- app/src/main/java/com/raeed/tictactoc/MainActivity.java
+++ app/src/main/java/com/raeed/tictactoc/MainActivity.java
@@ -8,7 +8,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String MAIN_PLAYER ="main_name";
@@ -46,5 +50,50 @@ public class MainActivity extends AppCompatActivity {
             player1.setText(R.string.player1);
             }
         }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.newgm:
+                Toast toast1=Toast.makeText(getBaseContext(),"New game option coming soon",Toast.LENGTH_LONG);
+                toast1.show();
+                // Todo New game option coming soon
+                //newGame();
+                return true;
+            case R.id.bakgnd:
+                Toast toast2=Toast.makeText(getBaseContext(),"Background change option coming soon",Toast.LENGTH_LONG);
+                toast2.show();
+                //backgroundChanger();
+                return true;
+            case R.id.historygm:
+                Toast toast3=Toast.makeText(getBaseContext(),"making history activity link",Toast.LENGTH_LONG);
+                toast3.show();
+                //todo making history activity link
+                return true;
+            case R.id.androidgm:
+                Toast toast4=Toast.makeText(getBaseContext(),"play with android",Toast.LENGTH_LONG);
+                toast4.show();
+                //todo play with android class
+                return true;
+            case R.id.humangm:
+                Toast toast5=Toast.makeText(getBaseContext(),"play with human",Toast.LENGTH_LONG);
+                toast5.show();
+                //Todo play with human class
+                return true;
+            case R.id.help:
+                Toast toast6=Toast.makeText(getBaseContext(),"help activity",Toast.LENGTH_LONG);
+                toast6.show();
+                //Todo play with human class
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
