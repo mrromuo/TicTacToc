1f77e5a8b3922e976bd12559982b94c85ca5fb6b
mrromuo <mrrmuo@gmail.com>
2020-08-08 04:48:08 +0300

    parson Activity added and desained activity_gamehistory.xml added menu_main.xml added Button background added in drawable Folder named(Burronbk) srings are updated

---
+++ app/src/main/java/com/raeed/tictactoc/gamehistory.java
@@ -0,0 +1,14 @@
+package com.raeed.tictactoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gamehistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gamehistory);
+    }
+}
