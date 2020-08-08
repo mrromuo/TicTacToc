51d10d9db3857e7c6c62ac28936d839c84d935e1
mrromuo <mrrmuo@gmail.com>
2020-08-08 14:45:45 +0300

    coding start!! coding for shared data of main user name

--- app/src/main/java/com/raeed/tictactoc/MainActivity.java
+++ app/src/main/java/com/raeed/tictactoc/MainActivity.java
@@ -1,14 +1,50 @@
 package com.raeed.tictactoc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String MAIN_PLAYER ="main_name";
+    public static final String NOT_YET ="NoName";
+    public static final int PERSON_INFO_REQUEST = 1;
+    public static String MAIN_PLAYER_V ;
+    public static String MAIN_PLAYER_NAME ;
+    TextView player1;
 
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        player1 = findViewById(R.id.player1);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        isNameVailable();
+        
+    }
+    private void isNameVailable () {
+        MAIN_PLAYER_V = sharedPreferences.getString(MAIN_PLAYER,NOT_YET);
+        if  (MAIN_PLAYER_V == NOT_YET) {
+            Intent infoperson =new Intent(getBaseContext(),person.class);
+            startActivityForResult(infoperson,PERSON_INFO_REQUEST);
+        } else {player1.setText(MAIN_PLAYER_V);}
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK & requestCode == PERSON_INFO_REQUEST ){
+            MAIN_PLAYER_NAME =data.getStringExtra(MAIN_PLAYER);
+            player1.setText(MAIN_PLAYER_NAME);
+        } else { if (resultCode == RESULT_CANCELED & requestCode == PERSON_INFO_REQUEST ){
+            player1.setText(R.string.player1);
+            }
+        }
     }
 }
--- app/src/main/java/com/raeed/tictactoc/person.java
+++ app/src/main/java/com/raeed/tictactoc/person.java
@@ -1,14 +1,52 @@
 package com.raeed.tictactoc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import static com.raeed.tictactoc.MainActivity.MAIN_PLAYER;
 
 public class person extends AppCompatActivity {
+    ImageView imageView;
+    EditText newname;
+    Button save;
+    Button cancel;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.test);
+        newname = findViewById(R.id.editPersonName);
+        save = findViewById(R.id.submit);
+        cancel = findViewById(R.id.cancel);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent();
+                String name = newname.getText().toString();
+                if (name != "") {
+                    intent.putExtra(MAIN_PLAYER,name);
+                    setResult(RESULT_OK,intent);
+                    finish();
+                }
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent();
+                setResult(RESULT_CANCELED,intent);
+                finish();
+            }
+        });
     }
+
 }
