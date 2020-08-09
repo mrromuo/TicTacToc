6f832b02b1ea5ad2c1a164507f7498511a2fb874
mrromuo <48122087+mrromuo@users.noreply.github.com>
2020-08-09 16:24:16 +0300

    key tipe changed to Image butoons editing butoons clikc

--- app/src/main/java/com/raeed/tictactoc/MainActivity.java
+++ app/src/main/java/com/raeed/tictactoc/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Set;
@@ -20,11 +21,16 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     public static final String MAIN_PLAYER ="main_name";
     public static final String NOT_YET ="NoName";
+    public static final String GAME_SQ_KEY ="gmaesq";
     public static final int PERSON_INFO_REQUEST = 1;
+    public static final int  O_samble_color= (R.color.image_backgroind_o);
+    public static final int X_samble_color = (R.color.image_backgroind_x);
     public static String MAIN_PLAYER_V ;
     public static String MAIN_PLAYER_NAME ;
+    public static int gamesequence;
     TextView player1,player2;
-    Button but1,but2,but3,but4,but5,but6,but7,but8,but9,rest;
+    ImageButton but1,but2,but3,but4,but5,but6,but7,but8,but9;
+    Button rest;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
     @Override
@@ -51,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
     private void isNameVailable () {
         MAIN_PLAYER_V = sharedPreferences.getString(MAIN_PLAYER,NOT_YET);
         if  (MAIN_PLAYER_V == NOT_YET) {
+            gamesequence=0;
+            editor.putInt(GAME_SQ_KEY,gamesequence);
+            editor.apply();
             Intent infoperson =new Intent(getBaseContext(),person.class);
             startActivityForResult(infoperson,PERSON_INFO_REQUEST);
         } else {player1.setText(MAIN_PLAYER_V);}
@@ -115,18 +124,57 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private class humangame{
-        void onClick(View view){
-            switch (view.getId()){
-                case R.id.bu1:
-                    but1.setBackgroundColor(getResources().getColor(R.color.image_backgroind_o));
-                    but1.setText("o");
-                    break;
-                case R.id.bu2:
-                    but2.setBackgroundColor(getResources().getColor(R.color.image_backgroind_o));
-                    but2.setText("o");
-                    break;
-            }
+    public void butclicked(View view) {
+        gamesequence =  sharedPreferences.getInt(GAME_SQ_KEY,0);
+        int SIMPLE_X_OR_O_color;
+        int playersimple=0;
+        if (gamesequence==0){
+            gamesequence=1;
+            SIMPLE_X_OR_O_color = O_samble_color;
+            playersimple=(R.drawable.o); }
+        else {
+            gamesequence=0;
+            SIMPLE_X_OR_O_color = X_samble_color;
+            playersimple=(R.drawable.x01);;}
+        switch (view.getId()) {
+            case R.id.bu1:
+                but1.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but1.setImageResource(playersimple);
+                break;
+            case R.id.bu2:
+                but2.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but2.setImageResource(playersimple);
+                break;
+            case R.id.bu3:
+                but3.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but3.setImageResource(playersimple);
+                break;
+            case R.id.bu4:
+                but4.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but4.setImageResource(playersimple);
+                break;
+            case R.id.bu5:
+                but5.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but5.setImageResource(playersimple);
+                break;
+            case R.id.bu6:
+                but6.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but6.setImageResource(playersimple);
+                break;
+            case R.id.bu7:
+                but7.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but7.setImageResource(playersimple);
+                break;
+            case R.id.bu8:
+                but8.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but8.setImageResource(playersimple);
+                break;
+            case R.id.bu9:
+                but9.setBackgroundColor(getResources().getColor(SIMPLE_X_OR_O_color));
+                but9.setImageResource(playersimple);
+                break;
         }
+        editor.putInt(GAME_SQ_KEY,gamesequence);
+        editor.apply();
     }
 }
