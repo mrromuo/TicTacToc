04b2c88071157dfcf9c1100d2754657334deb8d0
mrromuo <mrrmuo@gmail.com>
2020-08-13 16:15:24 +0300

    strings.xml: new strings for playing with Android or Human states Main Activity : Complete On Restore instant State function                 worked in menu selections like reset..

--- app/src/main/java/com/raeed/tictactoc/MainActivity.java
+++ app/src/main/java/com/raeed/tictactoc/MainActivity.java
@@ -1,12 +1,9 @@
 package com.raeed.tictactoc;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,25 +14,30 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String MAIN_PLAYER ="main_name";
-    public static final String NOT_YET ="NoName";
-    public static final String GAME_SQ_KEY ="gmaesq";
-    public static final String GAME_QOUNT_KEY ="gmqounter";
-    public static final String GAME_ANDROID_KEY ="gmandroid";
+    public static final String MAIN_PLAYER = "main_name";
+    public static final String NOT_YET = "NoName";
+    public static final String GAME_SQ_KEY = "gmaesq";
+    public static final String GAME_QOUNT_KEY = "gmqounter";
+    public static final String GAME_ANDROID_KEY = "gmandroid";
     public static final int PERSON_INFO_REQUEST = 1;
-    public static final int  O_samble_color= (R.color.image_backgroind_o);
+    public static final int O_samble_color = (R.color.image_backgroind_o);
     public static final int X_samble_color = (R.color.image_backgroind_x);
-    public static String MAIN_PLAYER_V ;
-    public static String MAIN_PLAYER_NAME ;
-    public static int gamesequence,GameCounter;
-    public static boolean PLAYER1_ST,PLAYER2_ST ,GAME_ANDROID;
-    public static boolean[][] GAME_BOARD =new boolean[2][9];
-    int[] kyMap ={R.id.bu1,R.id.bu2,R.id.bu3,R.id.bu4,R.id.bu5,R.id.bu6,R.id.bu7,R.id.bu8,R.id.bu9};
-    TextView player1,player2;
+    public static String MAIN_PLAYER_V;
+    public static String MAIN_PLAYER_NAME;
+    public static int gamesequence, GameCounter;
+    public static boolean PLAYER1_ST, PLAYER2_ST, GAME_ANDROID;
+    public static boolean[][] GAME_BOARD = new boolean[2][9];
+    int[] kyMap = {R.id.bu1, R.id.bu2, R.id.bu3, R.id.bu4, R.id.bu5, R.id.bu6, R.id.bu7, R.id.bu8, R.id.bu9};
+    TextView player1, player2;
     Button rest;
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,64 +57,64 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void isNameVailable () {
-        MAIN_PLAYER_V = sharedPreferences.getString(MAIN_PLAYER,NOT_YET);
-        if  (MAIN_PLAYER_V == NOT_YET) {
-            gamesequence=0;
-            editor.putInt(GAME_SQ_KEY,gamesequence);
+
+    private void isNameVailable() {
+        MAIN_PLAYER_V = sharedPreferences.getString(MAIN_PLAYER, NOT_YET);
+        if (MAIN_PLAYER_V == NOT_YET) {
+            gamesequence = 0;
+            editor.putInt(GAME_SQ_KEY, gamesequence);
             editor.apply();
-            Intent infoperson =new Intent(getBaseContext(),person.class);
-            startActivityForResult(infoperson,PERSON_INFO_REQUEST);
+            Intent infoperson = new Intent(getBaseContext(), person.class);
+            startActivityForResult(infoperson, PERSON_INFO_REQUEST);
         } else {
             player1.setText(MAIN_PLAYER_V);
-            MAIN_PLAYER_NAME = MAIN_PLAYER_V;}
+            MAIN_PLAYER_NAME = MAIN_PLAYER_V;
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK & requestCode == PERSON_INFO_REQUEST ){
-            MAIN_PLAYER_NAME =data.getStringExtra(MAIN_PLAYER);
+        if (resultCode == RESULT_OK & requestCode == PERSON_INFO_REQUEST) {
+            MAIN_PLAYER_NAME = data.getStringExtra(MAIN_PLAYER);
             player1.setText(MAIN_PLAYER_NAME);
-            editor.putString(MAIN_PLAYER,MAIN_PLAYER_NAME);
+            editor.putString(MAIN_PLAYER, MAIN_PLAYER_NAME);
             editor.apply();
-        } else { if (resultCode == RESULT_CANCELED & requestCode == PERSON_INFO_REQUEST ){
-            player1.setText(R.string.player1);
+        } else {
+            if (resultCode == RESULT_CANCELED & requestCode == PERSON_INFO_REQUEST) {
+                player1.setText(R.string.player1);
             }
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.newgm:
-                Toast toast1=Toast.makeText(getBaseContext(),"New game option coming soon",Toast.LENGTH_LONG);
-                toast1.show();
-                // Todo New game option coming soon
-                //newGame();
+                resetgame();
                 return true;
             case R.id.bakgnd:
-                Toast toast2=Toast.makeText(getBaseContext(),"Background change option coming soon",Toast.LENGTH_LONG);
+                Toast toast2 = Toast.makeText(getBaseContext(), "Background change option coming soon", Toast.LENGTH_LONG);
                 toast2.show();
                 //backgroundChanger();
                 return true;
             case R.id.historygm:
-                Toast toast3=Toast.makeText(getBaseContext(),"making history activity link",Toast.LENGTH_LONG);
+                Toast toast3 = Toast.makeText(getBaseContext(), "making history activity link", Toast.LENGTH_LONG);
                 toast3.show();
                 //todo making history activity link
                 return true;
             case R.id.androidgm:
-                Toast toast4=Toast.makeText(getBaseContext(),"play with android",Toast.LENGTH_LONG);
+                Toast toast4 = Toast.makeText(getBaseContext(), getText(R.string.playwithAndroid), Toast.LENGTH_LONG);
                 toast4.show();
-                //todo play with android class
+                GAME_ANDROID=true;
                 return true;
             case R.id.humangm:
-                Toast toast5=Toast.makeText(getBaseContext(),"play with human",Toast.LENGTH_LONG);
-                toast5.show();
-                //Todo play with human class
+                GAME_ANDROID=false;
+                Toast.makeText(getBaseContext(), getText(R.string.playwithHuman), Toast.LENGTH_LONG).show();
                 return true;
             case R.id.help:
-                Toast toast6=Toast.makeText(getBaseContext(),"help activity",Toast.LENGTH_LONG);
+                Toast toast6 = Toast.makeText(getBaseContext(), "help activity", Toast.LENGTH_LONG);
                 toast6.show();
                 //Todo play with human class
                 return true;
@@ -120,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -129,18 +132,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void butclicked(View view) {
         // TODO make sprouted auto replay game with Android
-        gamesequence =  sharedPreferences.getInt(GAME_SQ_KEY,0);
+        gamesequence = sharedPreferences.getInt(GAME_SQ_KEY, 0);
         int SIMPLE_X_OR_O_color;
-        int playersimple=0;
-        GameCounter ++;
-        if (gamesequence==0){
-            gamesequence=1;
+        int playersimple = 0;
+        GameCounter++;
+        if (gamesequence == 0) {
+            gamesequence = 1;
             SIMPLE_X_OR_O_color = O_samble_color;
-            playersimple=(R.drawable.o); }
-        else {
-            gamesequence=0;
+            playersimple = (R.drawable.o);
+        } else {
+            gamesequence = 0;
             SIMPLE_X_OR_O_color = X_samble_color;
-            playersimple=(R.drawable.x);;}
+            playersimple = (R.drawable.x);
+            ;
+        }
         if (GameCounter < 10) {
             int but = view.getId();
             ImageButton presdBut = findViewById(but);
@@ -151,38 +156,47 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.bu1:
                     GAME_BOARD[gamesequence][0] = true;
                     editor.putBoolean("s0", true);
+                    editor.putInt("xo0", gamesequence);
                     break;
                 case R.id.bu2:
                     GAME_BOARD[gamesequence][1] = true;
                     editor.putBoolean("s1", true);
+                    editor.putInt("xo1", gamesequence);
                     break;
                 case R.id.bu3:
                     GAME_BOARD[gamesequence][2] = true;
                     editor.putBoolean("s2", true);
+                    editor.putInt("xo2", gamesequence);
                     break;
                 case R.id.bu4:
                     GAME_BOARD[gamesequence][3] = true;
                     editor.putBoolean("s3", true);
+                    editor.putInt("xo3", gamesequence);
                     break;
                 case R.id.bu5:
                     GAME_BOARD[gamesequence][4] = true;
                     editor.putBoolean("s4", true);
+                    editor.putInt("xo4", gamesequence);
                     break;
                 case R.id.bu6:
                     GAME_BOARD[gamesequence][5] = true;
                     editor.putBoolean("s5", true);
+                    editor.putInt("xo5", gamesequence);
                     break;
                 case R.id.bu7:
                     GAME_BOARD[gamesequence][6] = true;
                     editor.putBoolean("s6", true);
+                    editor.putInt("xo6", gamesequence);
                     break;
                 case R.id.bu8:
                     GAME_BOARD[gamesequence][7] = true;
                     editor.putBoolean("s7", true);
+                    editor.putInt("xo7", gamesequence);
                     break;
                 case R.id.bu9:
                     GAME_BOARD[gamesequence][8] = true;
                     editor.putBoolean("s8", true);
+                    editor.putInt("xo8", gamesequence);
                     break;
             }
             // find winner
@@ -194,13 +208,11 @@ public class MainActivity extends AppCompatActivity {
                 PLAYER2_ST = GAME_BOARD[0][secdS[s]] & GAME_BOARD[0][secdS[s + 1]] & GAME_BOARD[0][secdS[s + 2]];
                 PLAYER1_ST = GAME_BOARD[1][secdS[s]] & GAME_BOARD[1][secdS[s + 1]] & GAME_BOARD[1][secdS[s + 2]];
                 if (PLAYER1_ST) {
-                    winner = "A is winner";
-                    GameCounter =10;
+                    GameCounter = 10;
                     winner(1);
                     break;
                 } else if (PLAYER2_ST) {
-                    GameCounter =10;
-                    winner = "B is winner";
+                    GameCounter = 10;
                     winner(2);
                     break;
                 } else winner = "Draw";
@@ -208,40 +220,79 @@ public class MainActivity extends AppCompatActivity {
                 x++;
             } while (x < 8);
         }
-        editor.putInt(GAME_SQ_KEY,gamesequence);
-        editor.putInt(GAME_QOUNT_KEY,GameCounter);
+        editor.putInt(GAME_SQ_KEY, gamesequence);
+        editor.putInt(GAME_QOUNT_KEY, GameCounter);
         editor.apply();
     }
 
-    private void resetgame(){
-        String sX[] = {"s0","s1","s2","s3","s4","s5","s6","s7","s8"};
-        int x=0;
-        do{
-            for(int i =0;i<9;i++)GAME_BOARD[x][i]=false;
+    private void resetgame() {
+        String sX[] = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"};
+        int x = 0;
+        do {
+            for (int i = 0; i < 9; i++) GAME_BOARD[x][i] = false;
             x++;
-        }while (x<2);
-        gamesequence=0;
-        GameCounter=0;
-        for(int i =0;i<9;i++){
-            ImageButton button=findViewById(kyMap[i]);
-            button.setBackgroundColor(getResources().getColor(R.color.colorwhite,getBaseContext().getTheme()));
+        } while (x < 2);
+        gamesequence = 0;
+        GameCounter = 0;
+        for (int i = 0; i < 9; i++) {
+            ImageButton button = findViewById(kyMap[i]);
+            button.setBackgroundColor(getResources().getColor(R.color.colorwhite, getBaseContext().getTheme()));
             button.setImageResource(0);
             button.setClickable(true);
-            editor.putBoolean(sX[i],false);}
-        editor.putInt(GAME_SQ_KEY,gamesequence);
-        editor.putInt(GAME_QOUNT_KEY,GameCounter);
+            editor.putBoolean(sX[i], false);
+        }
+        editor.putInt(GAME_SQ_KEY, gamesequence);
+        editor.putInt(GAME_QOUNT_KEY, GameCounter);
         editor.apply();
     }
 
     // winner announcement
-    private void winner(int playerId){
-        String p1=getText(R.string.player1win).toString()+ " " + MAIN_PLAYER_NAME;
-        String p2=getText(R.string.player2win).toString();
-        if (playerId == 1){
-            Toast.makeText(getBaseContext(),p1,Toast.LENGTH_LONG).show();
+    private void winner(int playerId) {
+        String p1 = getText(R.string.player1win).toString() + " " + MAIN_PLAYER_NAME;
+        String p2 = getText(R.string.player2win).toString();
+        if (playerId == 1) {
+            Toast.makeText(getBaseContext(), p1, Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(getBaseContext(),p2,Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), p2, Toast.LENGTH_LONG).show();
         }
         // TODO Make activity for winner announcement
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt(GAME_SQ_KEY, gamesequence);
+        outState.putInt(GAME_QOUNT_KEY, GameCounter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        gamesequence = sharedPreferences.getInt(GAME_SQ_KEY, 0);
+        GameCounter = sharedPreferences.getInt(GAME_QOUNT_KEY, 0);
+        String xo[] = {"xo0", "xo1", "xo2", "xo3", "xo4", "xo5", "xo6", "xo7", "xo8"};
+        String sX[] = {"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9"};
+
+        for (int i = 0; i < 9; i++) {
+            ImageButton button = findViewById(kyMap[i]);
+            boolean satatus = sharedPreferences.getBoolean(sX[i], false);
+            int theXO = sharedPreferences.getInt(xo[i], 0);
+            GAME_BOARD[theXO][i]=satatus;
+            if (satatus) {
+                if (theXO == 0) {
+                    button.setBackgroundColor(getResources().getColor(R.color.image_backgroind_x, getBaseContext().getTheme()));
+                    button.setImageResource(R.drawable.x);
+                    button.setClickable(false);
+                } else {
+                    button.setBackgroundColor(getResources().getColor(R.color.image_backgroind_o, getBaseContext().getTheme()));
+                    button.setImageResource(R.drawable.o);
+                    button.setClickable(false);
+                }
+            } else {
+                button.setBackgroundColor(getResources().getColor(R.color.colorwhite, getBaseContext().getTheme()));
+                button.setImageResource(0);
+                button.setClickable(true);
+            }
+        }
     }
 }
